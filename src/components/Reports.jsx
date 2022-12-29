c7964cd48f8f919676c4589fdbd832512a6c2fe5
BuildTools <ayoubzitouni315@gmail.com>
2022-12-29 10:42:54 +0100

    update 0.3

--- src/components/Reports.jsx
+++ src/components/Reports.jsx
@@ -5,18 +5,14 @@ import Cards from './Cards'
 
 const Reports = () => {
   const dispatch=useDispatch()
-  const {reports}=useSelector(state=>state.getReports)
-  const [rep,setRep]=useState()
+  const report=useSelector(state=>state.getReports)
   const {userId}=JSON.parse(localStorage.getItem("auth"))
   useEffect(() => {
     dispatch(getMedicalRep(userId))
-    setRep(reports)
-  }, [])
-  
-
+  },[])
   return (
     <div style={{width:"100%",display:"flex",flexDirection:"column",alignItems:"center",padding:"5px"}}>
-      {rep && rep.map((el)=> <Cards reports={el} />)}
+      {report && !report.reports?<h1>No reports</h1>:report.reports.length>1?report.reports.map((el)=><Cards reports={el}/>):<h1>No reports</h1>}
     </div>
   )
 }
